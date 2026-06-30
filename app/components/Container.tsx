3e3a04f1963a6ccb7cb8d387a033c76343c929f2
jboori <m7mdjbr1426@gmail.com>
2026-06-30 14:22:10 +0300

    intial commit

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,4 +1,6 @@
-export default function Container({ children }) {
+import { ReactNode } from "react";
+
+export default function Container({ children }: { children: ReactNode }) {
   return (
     <div className="w-screen mx-auto px-2 md:px-12 lg:px-20">{children}</div>
   );
